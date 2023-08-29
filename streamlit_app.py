8fc8bbfa234a3af892f6a2cbecb59015e5de659c
buravelliprasad <138657173+buravelliprasad@users.noreply.github.com>
2023-08-29 10:18:50 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -67,7 +67,8 @@ tool = create_retriever_tool(
     "Searches and returns documents regarding the car inventory."
 )
 tools = [tool]
-llm = OpenAI(model="gpt-3.5",temperature=0)
+llm = ChatOpenAI(model="gpt-3.5-turbo-16k", temperature = 0)
+# llm = OpenAI(model="gpt-3.5",temperature=0)
 memory_key = "history"
 from langchain.agents.openai_functions_agent.agent_token_buffer_memory import AgentTokenBufferMemory
 memory = AgentTokenBufferMemory(memory_key=memory_key, llm=llm)
